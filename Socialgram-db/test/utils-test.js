dfd9b67b8a1e3a0fb1d4d21dac36a9038aabc4af
Hector Riquelme <hector.riquelme1169@gmail.com>
2017-03-28 21:56:58 -0300

    Se creo el test para la validacion en la extraccion de hashtag

--- Socialgram-db/test/utils-test.js
+++ Socialgram-db/test/utils-test.js
@@ -2,18 +2,26 @@
 
 const test = require('ava')
 
+const utils = require('../lib/utils');
 
-test('This should pass', t => {
-	t.pass()
-})
+test('extracting hashtag from text', t => {
+	let tags = utils.extractTags('a #picture with tags #Awesome #Socialgram ##Nice');
 
+	t.deepEqual(tags,[
+		'picture',
+		'awesome',
+		'socialgram',
+		'nice'
+	])
 
-test('This should fail', t => {
-	t.fail()
-})
+	tags = utils.extractTags('a picture with no tags')
+	t.deepEqual(tags, [])
 
-test('It should support async/await', async t => {
-	let p = Promise.resolve(42)
-	let secret = await p
-	t.is(secret, 42)
-})
+	tags = utils.extractTags();
+	t.deepEqual(tags, []);	
+
+	tags = utils.extractTags(null);
+	t.deepEqual(tags, []);	
+
+});
+
