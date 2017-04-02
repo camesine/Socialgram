ec43c672adacedbfdf8beb27c2a8f29e0592ef9a
Hector Riquelme <hector.riquelme1169@gmail.com>
2017-04-01 23:14:38 -0300

    Test de creacion de registros

--- Socialgram-db/lib/db.js
+++ Socialgram-db/lib/db.js
@@ -25,35 +25,52 @@ class Db{
 			port: this.port
 		})
 
+		this.connected = true
+
 		let db = this.db
 		let connection = this.connection
 
 		let setup = co.wrap(function * (){
 			let conn = yield connection
 			
-			console.log("paso1")
+			
 			let dbList = yield r.dbList().run(conn)
 			if(dbList.indexOf(db) === -1)
 				yield r.dbCreate(db).run(conn)
 
-			console.log("paso2")
+			
 
 			let dbTables = yield r.db(db).tableList().run(conn)
 			if(dbTables.indexOf('images') === -1)
 				yield r.db(db).tableCreate('images').run(conn)
 			
-			console.log("paso3")
+			
 
 			if(dbTables.indexOf('users') === -1)
 				yield r.db(db).tableCreate('users').run(conn)
 
-			console.log("paso4")
+			
 			return conn
 
 		})
 
-		console.log("paso5")
+		
 		return Promise.resolve(setup()).asCallback(callback)
+
+	}
+
+	disconnect (callback) {
+		if (!this.connected)
+			return Promise.reject(new Error('not connected')).asCallback(callback)
+
+		this.connected = false
+		return Promise.resolve(this.connection)
+			.then((conn) => conn.close())
+	}
+
+	saveImage(image, callback){
+		if (!this.connected)
+			return Promise.reject(new Error('not connected')).asCallback(callback)
 
 	}
 }
