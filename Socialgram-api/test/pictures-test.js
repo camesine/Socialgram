c132237bf3aa70f7de5cfc36c1bdbf3245fe87b2
Hector Riquelme <hector.riquelme1169@gmail.com>
2017-04-05 23:17:12 -0300

    Creacion de socialgram-api

---
+++ Socialgram-api/test/pictures-test.js
@@ -0,0 +1,20 @@
+'use strict'
+
+import test from 'ava'
+import micro, {send} from 'micro'
+import uuid from 'uuid-base62'
+import listen from 'test-listen'
+import request from 'request-promise'
+
+test('GET /:id', async t => {
+	let id = uuid.v4()
+
+	let srv = micro(async(req, res) => {
+		send(res, 200, {id})
+	})
+
+	let url = await list(srv)
+	let body = await request({uri: url, json: true})
+
+	t.deepEqual(body, {id})
+})
